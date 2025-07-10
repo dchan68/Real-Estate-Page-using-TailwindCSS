4f6bdcc6087014f6c1f256f8233b625f8480e439
Daryan Chan <daryan.j.chan@hotmail.com>
2025-07-10 15:17:53 -0400

    finished header and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { assets } from "../assets/assets";
 
 function Navbar() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  useEffect(() => {
+    if (showMobileMenu) {
+      //will stop page from being able to scroll if menu is open
+      document.body.style.overflow = "hidden";
+    } else {
+      //allow us to scroll web page
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showMobileMenu]);
+
   return (
-    <div className="absolute top-0 left-0 w-full z-10">
+    <div className="absolute top-3 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between item-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
         <img src={assets.logo} alt="" />
         <ul className="hidden md:flex gap-7 text-white">
@@ -20,9 +35,60 @@ function Navbar() {
             Testimonials
           </a>
         </ul>
-        <button className="hidden md:block bg-white px-8 py-2 rounded full">
+        <button className="hidden md:block bg-white px-8 py-2 rounded-full">
           Sign Up
         </button>
+        <img
+          onClick={() => setShowMobileMenu(true)}
+          src={assets.menu_icon}
+          className="md:hidden w-7 cursor-pointer"
+          alt=""
+        />
+      </div>
+      {/* mobile menu */}
+      <div
+        className={`md:hidden ${
+          showMobileMenu ? "fixed w-full" : "h-0 w-0"
+        } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+          <div className="flex justify-end p-6 cursor-pointer">
+            <img
+              onClick={() => setShowMobileMenu(false)}
+              src={assets.cross_icon}
+              className="w-6"
+              alt=""
+            />
+          </div>
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#Header"
+            className="px-4 py-2 rounded-full inline-blockl"
+          >
+            Home
+          </a>
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#About"
+            className="px-4 py-2 rounded-full inline-blockl"
+          >
+            About
+          </a>
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#Projects"
+            className="px-4 py-2 rounded-full inline-blockl"
+          >
+            Projects
+          </a>
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#Testimonials"
+            className="px-4 py-2 rounded-full inline-blockl"
+          >
+            Testimonials
+          </a>
+        </ul>
       </div>
     </div>
   );
