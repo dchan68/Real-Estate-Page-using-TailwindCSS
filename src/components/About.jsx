b529961aae014e6d4878763e01e9ee952d74a5f7
Daryan Chan <daryan.j.chan@hotmail.com>
2025-07-10 15:22:10 -0400

    started on about component

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      id="About"
+      className="flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+        About <span>Our Brand</span>
+      </h1>
+      <p>Passionate About Properties, Ddeciated To Your Vision</p>
+    </div>
+  );
+}
+
+export default About;
