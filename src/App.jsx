bb7d66bfe718a7e9206cec46687a40c550916871
Daryan Chan <daryan.j.chan@hotmail.com>
2025-07-15 11:22:29 -0400

    finished contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
       <Testimonials />
+      <Contact />
     </div>
   );
 }
